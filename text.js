f336adffd3829168e20ee7e8f0518e7d27daac8e
christina <cbernard2010@gmail.com>
2017-04-23 08:27:55 -0500

    send text v19

--- text.js
+++ text.js
@@ -1,29 +1,5 @@
 
-/*
-function sentTextMessage(){
-    var accountSid ='AC4851e7938c2970194bcc3821db6ff655';
-    var authToken ='64d897277252702e1671cce391870663';
-
-    var client = require('twilio')(accountSid,authToken);
-
-
-    client.messages.create({
-
-        to: "+17133576031",
-        from: "+16198701485",
-        body: "Mary Smith 555444333 is route to Station ",
-    }, function(err,message){
-        if(err){
-            console.log(err);
-        }else{
-         console.log(message.sid);
-         }
-    });
-
-}
-*/
-
-var sentTextMessage= function(){
+function sendTextMessage() {
     var accountSid ='AC4851e7938c2970194bcc3821db6ff655';
     var authToken ='64d897277252702e1671cce391870663';
 
@@ -43,7 +19,3 @@ var sentTextMessage= function(){
          }
     });
 }
-$(window).load(function(){
-    $("#sendText").submit(sendTextMessage);
-
-});
