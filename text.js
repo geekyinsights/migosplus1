f66fa8dae017de892ff8b42382af4f1438e84fc6
christina <cbernard2010@gmail.com>
2017-04-23 06:05:51 -0500

    take picure test1

--- text.js
+++ text.js
@@ -1,19 +1,3 @@
-
-var accountSid ='AC4851e7938c2970194bcc3821db6ff655';
-var authToken ='64d897277252702e1671cce391870663';
-
-var twilio = require('twilio')
-
-var client = new twilio.RestClient(accountSid,authToken);
-var followTrip = function() {
-client.messages.create({
-    body: 'Mary Smith 555444333 is route to Station ',
-    to: +17133576031,
-    from: +16018963851,
-}, function(err,message){
-    console.log(message.sid);
-});
-
 $(window).load(function () {
 
   // Find the HTML element with the id recommendationForm, and when the submit
@@ -21,7 +5,6 @@ $(window).load(function () {
   $("#folTrip").submit(followTrip);
 
 });
-
 /*
 // Download the twilio-csharp library from twilio.com/docs/libraries/csharp
 using System;
