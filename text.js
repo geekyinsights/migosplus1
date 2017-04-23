34de2d787c5b9c35807eec13d4c1f7d20c707311
christina <cbernard2010@gmail.com>
2017-04-23 05:02:02 -0500

    working google nav recieve sms test1

--- text.js
+++ text.js
@@ -1,26 +1,20 @@
-users.limitToLast(1).on('child_added', function(childSnapshot){
-    user = childSnapshot.val();
+var accountSid ='AC355bbbd5431e7f69a73c32163563f1f5';
+var authToken ='b932ab0663de9a4bc734fdfe0d03b98f';
 
-    $("#firstName").html(user.firstName)
-    $("#lastName").html(user.lastName)
-    $("#email").html(user.email)
-    $("#phone").html(user.phone)
-    $("#license").html(user.license)
-
-});
-
-
-
-var accountSid ='{{AC355bbbd5431e7f69a73c32163563f1f5}}';
-var authToken ='{{b932ab0663de9a4bc734fdfe0d03b98f}}';
-
-var twilio = require('twilio');
-var client = new twilio.RestClient(accountSid,authToken);
-
+var twilio = require('twilio')(accountSid,authToken);
+var followTrip = function() {
 client.messages.create({
-    body: user.firstName + user.lastName + 'is route to Station ',
-    to: station.number,
-    from: user.phone,
+    body: 'Mary Smith 555444333 is route to Station ',
+    to: +17133576031,
+    from: +16018963851,
 }, function(err,message){
     console.log(message.sid);
+});
+
+$(window).load(function () {
+
+  // Find the HTML element with the id recommendationForm, and when the submit
+  // event is triggered on that element, call submitRecommendation.
+  $("#folTrip").submit(followTrip);
+
 });
